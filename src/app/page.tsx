d24290842df0010a11a1de8c78c1908d85078d97
artemchs <artemchs123@gmail.com>
2024-11-02 10:54:57 +0100

    feat: :lipstick: set up mantine with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@mantine/core";
+
 export default function Page() {
-  return <></>;
+  // `bg-red-500` will be `background-color: var(--mantine-color-red-5)`
+  // `text-white` will be `color: var(--mantine-color-white)`
+  return <Button className="bg-green-500 text-white">Hello</Button>;
 }
