3f2b5dbe1df4e6e0a11aceb2ac070529d8e09c57
Artem Chernysh <artemchs123@gmail.com>
2024-12-20 18:26:22 +0100

    fix: :bug: asdf

--- src/app/admin/goods/_components/table/columns.tsx
+++ src/app/admin/goods/_components/table/columns.tsx
@@ -1,43 +1,63 @@
 "use client";
 
+import { Box, Image } from "@mantine/core";
 import { createMRTColumnHelper } from "mantine-react-table";
 import { DisplayDate } from "~/components/DisplayDate";
+import { env } from "~/env";
 import { type RouterOutputs } from "~/trpc/react";
 
 const columnHelper =
-  createMRTColumnHelper<RouterOutputs["goods"]["readMany"]["items"][number]>();
+    createMRTColumnHelper<
+        RouterOutputs["goods"]["readMany"]["items"][number]
+    >();
 
 export const goodsColumns = [
-  columnHelper.accessor((row) => row.name, {
-    header: "Наименование",
-    id: "name",
-  }),
-  columnHelper.accessor((row) => row.sku, {
-    header: "Артикул",
-    id: "sku",
-  }),
-  columnHelper.accessor((row) => row.fullPrice, {
-    header: "Полная цена",
-    id: "fullPrice",
-  }),
-  columnHelper.accessor((row) => row.price, {
-    header: "Цена",
-    id: "price",
-  }),
-  columnHelper.accessor((row) => row.quantity, {
-    header: "Количество",
-    id: "quantity",
-  }),
-  columnHelper.accessor((row) => row.createdAt, {
-    header: "Дата создания",
-    Cell: ({ row }) => <DisplayDate date={row.original.updatedAt} />,
-    filterVariant: "date-range",
-    id: "createdAt",
-  }),
-  columnHelper.accessor((row) => row.updatedAt, {
-    header: "Дата обновления",
-    Cell: ({ row }) => <DisplayDate date={row.original.updatedAt} />,
-    filterVariant: "date-range",
-    id: "updatedAt",
-  }),
+    columnHelper.accessor((row) => row.name, {
+        header: "Название",
+        id: "name",
+        Cell: ({ row }) => {
+            const imageUrl = `https://${env.NEXT_PUBLIC_CLOUDFRONT_HOSTNAME}/${row.original.mediaToGood[0]?.media.key}`;
+
+            return (
+                <Box className="flex items-center gap-2">
+                    <Image
+                        src={imageUrl}
+                        alt={row.original.name}
+                        width={50}
+                        height={50}
+                        fit="cover"
+                    />
+                    {row.original.name}
+                </Box>
+            );
+        },
+    }),
+    columnHelper.accessor((row) => row.sku, {
+        header: "Артикул",
+        id: "sku",
+    }),
+    columnHelper.accessor((row) => row.fullPrice, {
+        header: "Полная цена",
+        id: "fullPrice",
+    }),
+    columnHelper.accessor((row) => row.price, {
+        header: "Цена",
+        id: "price",
+    }),
+    columnHelper.accessor((row) => row.quantity, {
+        header: "Количество",
+        id: "quantity",
+    }),
+    columnHelper.accessor((row) => row.createdAt, {
+        header: "Дата создания",
+        Cell: ({ row }) => <DisplayDate date={row.original.updatedAt} />,
+        filterVariant: "date-range",
+        id: "createdAt",
+    }),
+    columnHelper.accessor((row) => row.updatedAt, {
+        header: "Дата обновления",
+        Cell: ({ row }) => <DisplayDate date={row.original.updatedAt} />,
+        filterVariant: "date-range",
+        id: "updatedAt",
+    }),
 ];
--- src/server/actions/goods/readManyGoods.ts
+++ src/server/actions/goods/readManyGoods.ts
@@ -2,88 +2,99 @@ import { type PrismaTransaction } from "~/server/db";
 import { type Prisma } from "@prisma/client";
 import { mapColumnFilterToPrismaCondition } from "~/utils/table/mapColumnFilterToPrismaCondition";
 import type {
-  ReadManyGoodsInfiniteInput,
-  ReadManyGoodsInput,
+    ReadManyGoodsInfiniteInput,
+    ReadManyGoodsInput,
 } from "~/utils/validation/goods/readManyGoods";
 
 export const readManyGoods = async ({
-  tx,
-  payload,
+    tx,
+    payload,
 }: {
-  tx: PrismaTransaction;
-  payload: ReadManyGoodsInput;
+    tx: PrismaTransaction;
+    payload: ReadManyGoodsInput;
 }) => {
-  // Convert filters to Prisma conditions
-  const filterConditions =
-    payload.columnFilters?.map(({ id, value }) =>
-      mapColumnFilterToPrismaCondition({
-        id,
-        value,
-        columnFilterFn: payload.columnFilterFns[id] ?? "equals",
-      })
-    ) || [];
+    // Convert filters to Prisma conditions
+    const filterConditions =
+        payload.columnFilters?.map(({ id, value }) =>
+            mapColumnFilterToPrismaCondition({
+                id,
+                value,
+                columnFilterFn: payload.columnFilterFns[id] ?? "equals",
+            })
+        ) || [];
 
-  // Add global filter if present
-  if (payload.globalFilter) {
-    filterConditions.push({
-      name: { contains: payload.globalFilter, mode: "insensitive" },
-    });
-  }
+    // Add global filter if present
+    if (payload.globalFilter) {
+        filterConditions.push({
+            name: { contains: payload.globalFilter, mode: "insensitive" },
+        });
+    }
 
-  const where: Prisma.GoodWhereInput = {
-    AND: filterConditions.length > 0 ? filterConditions : undefined,
-  };
+    const where: Prisma.GoodWhereInput = {
+        AND: filterConditions.length > 0 ? filterConditions : undefined,
+    };
 
-  // Build orderBy from sorting
-  const orderBy = payload.sorting.map((sort) => ({
-    [sort.id]: sort.desc ? "desc" : "asc",
-  }));
+    // Build orderBy from sorting
+    const orderBy = payload.sorting.map((sort) => ({
+        [sort.id]: sort.desc ? "desc" : "asc",
+    }));
 
-  const [items, total] = await Promise.all([
-    tx.good.findMany({
-      skip: payload.pagination.pageIndex * payload.pagination.pageSize,
-      take: payload.pagination.pageSize,
-      where,
-      orderBy,
-    }),
-    tx.good.count({ where }),
-  ]);
+    const [items, total] = await Promise.all([
+        tx.good.findMany({
+            skip: payload.pagination.pageIndex * payload.pagination.pageSize,
+            take: payload.pagination.pageSize,
+            where,
+            orderBy,
+            include: {
+                mediaToGood: {
+                    orderBy: {
+                        index: "asc",
+                    },
+                    include: {
+                        media: true,
+                    },
+                    take: 1,
+                },
+            },
+        }),
+        tx.good.count({ where }),
+    ]);
 
-  return {
-    items,
-    total,
-    pageCount: Math.ceil(total / payload.pagination.pageSize),
-  };
+    return {
+        items,
+        total,
+        pageCount: Math.ceil(total / payload.pagination.pageSize),
+    };
 };
 
 export const readManyGoodsInfinite = async ({
-  tx,
-  payload,
+    tx,
+    payload,
 }: {
-  tx: PrismaTransaction;
-  payload: ReadManyGoodsInfiniteInput;
+    tx: PrismaTransaction;
+    payload: ReadManyGoodsInfiniteInput;
 }) => {
-  const items = await tx.good.findMany({
-    take: payload.limit + 1,
-    where: {
-      sku: {
-        contains: payload.globalFilter,
-        mode: "insensitive",
-      },
-    },
-    cursor: payload.cursor ? { id: payload.cursor } : undefined,
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+    const items = await tx.good.findMany({
+        take: payload.limit + 1,
+        where: {
+            sku: {
+                contains: payload.globalFilter,
+                mode: "insensitive",
+            },
+        },
+        cursor: payload.cursor ? { id: payload.cursor } : undefined,
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
 
-  let nextCursor: typeof payload.cursor | undefined = undefined;
-  if (items.length > payload.limit) {
-    const nextItem = items.pop();
-    nextCursor = nextItem!.id;
-  }
-  return {
-    items,
-    nextCursor,
-  };
+    let nextCursor: typeof payload.cursor | undefined = undefined;
+    if (items.length > payload.limit) {
+        const nextItem = items.pop();
+        nextCursor = nextItem!.id;
+    }
+    return {
+        items,
+        nextCursor,
+    };
 };
